4c2df964eaa242628c7613a9cc7aa85f9fe6aa60
Andreas Amsenius <andreas@amsenius.se>
2025-04-02 16:20:33 +0200

    custom debounce

--- keyboards/gmmk/pro/rev1/iso/keymaps/adbl/rules.mk
+++ keyboards/gmmk/pro/rev1/iso/keymaps/adbl/rules.mk
@@ -1,2 +1,6 @@
 CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = yes         # Commands for debug and configuration
+DEBOUNCE_TYPE = custom
+
+# Add debounce.c to the build
+SRC += debounce.c
