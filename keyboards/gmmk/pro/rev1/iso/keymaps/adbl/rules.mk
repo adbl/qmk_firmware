dab0aa8b3151037c22d1fa4729e14239cb1f8381
Andreas Amsenius <andreas@amsenius.se>
2025-04-03 09:54:30 +0200

    fix debounce

--- keyboards/gmmk/pro/rev1/iso/keymaps/adbl/rules.mk
+++ keyboards/gmmk/pro/rev1/iso/keymaps/adbl/rules.mk
@@ -1,6 +1,4 @@
 CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = yes         # Commands for debug and configuration
-DEBOUNCE_TYPE = custom
 
-# Add debounce.c to the build
-SRC += debounce.c
+DEBOUNCE_TYPE = asym_eager_defer_pk
